60c77e64ef592059d908f50626f637ae25bc1390
Talita Evancely <evancely@gmail.com>
2025-04-23 22:35:45 -0300

    Criando uma tela de resultado para o app gorjeta

--- app/src/main/java/com/talita/appgorjeta/MainActivity.kt
+++ app/src/main/java/com/talita/appgorjeta/MainActivity.kt
@@ -1,29 +1,13 @@
+
 package com.talita.appgorjeta
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputEditText
 import com.talita.appgorjeta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    /* valor total da conta
-     numero de pessoas
-     porcentagem da gorgeta
-     10%, 15% ou 20 %
-     botão calcular e limpar
-     Recuperar as Views do layout
-      - view binding
-      -recuperar as radios buttons
-      - calcular as porcetagens
-      - mostrar resultado na tela
-     */
 
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,24 +19,19 @@ class MainActivity : AppCompatActivity() {
         var porcentage: Int = 0
 
         binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
-        if (isChecked)
-        porcentage = 10
+            if (isChecked)
+                porcentage = 10
         }
 
         binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
-        if (isChecked)
-        porcentage = 15
+            if (isChecked)
+                porcentage = 15
 
         }
 
         binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
-        if (isChecked)
-        porcentage = 20
-
-        }
-        binding.btnClean.setOnClickListener{
-            println("talita 1" + binding.tieTotal.text)
-            println("talita 1" + binding.tieNumPeople.text)
+            if (isChecked)
+                porcentage = 20
 
         }
 
@@ -70,14 +49,14 @@ class MainActivity : AppCompatActivity() {
                 val Totaltable: Float = totalTabletemp.toString().toFloat()
                 val nPeople: Int = NPeopletemp.toString().toInt()
 
-                val TotalTemp = Totaltable / nPeople
-                val Tips = TotalTemp * porcentage /100
-                val TotalWithTips = TotalTemp + Tips
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("totalConta", Totaltable)
+                intent.putExtra("numeroPessoas", nPeople)
+                intent.putExtra("porcentagem", porcentage)
 
-                binding.tvResult.text = "conta com gorgeta : $TotalWithTips"
+                startActivity(intent)
             }
             binding.btnClean.setOnClickListener{
-                binding.tvResult.text = ""
                 binding.tieTotal.setText ("")
                 binding.tieNumPeople.setText ("")
                 binding.rbOptionOne.isChecked = false
